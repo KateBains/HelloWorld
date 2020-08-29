ef678476a3108fa74dc15b102307e429f82ed7c1
KateBains <katebains1@gmail.com>
2020-08-29 13:54:40 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hey you");
+            Console.WriteLine("What!-_-");
+            Console.WriteLine("Why you eating my icecream");
+            Console.WriteLine("   /|");
+            Console.WriteLine("  / |");
+            Console.WriteLine(" /  |");
+            Console.WriteLine("/___|");
+            Console.WriteLine("Buy me a new now!");
+            Console.WriteLine("Nope i have no money");
+            Console.WriteLine("I'm calling the police!");
+            Console.WriteLine("are you CRAZY fine then i'm buying a new now*-*");
+            Console.ReadLine();
+        }
+    }
+}
